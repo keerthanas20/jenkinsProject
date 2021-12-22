ef900b7639a712f10ac9ff455299e8ddeb989f11
Keerthana S <keerthanasamy20@gmail.com>
2021-12-22 08:31:18 +0530

    second commit

--- src/main/java/com/example/demo/JenkinsProjectApplication.java
+++ src/main/java/com/example/demo/JenkinsProjectApplication.java
@@ -3,19 +3,18 @@ package com.example.demo;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import lombok.extern.slf4j.Slf4j;
 
 
 
 
 @SpringBootApplication
-
+@Slf4j
 public class JenkinsProjectApplication {
-  static Logger log=(Logger) LoggerFactory.getLogger(JenkinsProjectApplication.class);
+  //static Logger log=(Logger) LoggerFactory.getLogger(JenkinsProjectApplication.class);
 	
   @PostConstruct
   public void inti() {
