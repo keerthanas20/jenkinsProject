b20a499d3c4ad2a8e56a72a54544c4f75b18adb1
Keerthana S <keerthanasamy20@gmail.com>
2021-12-22 08:38:18 +0530

    test folder changes

--- src/test/java/com/example/demo/JenkinsProjectApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsProjectApplicationTests.java
@@ -2,14 +2,19 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 class JenkinsProjectApplicationTests {
-	static Logger log=(Logger) LoggerFactory.getLogger(JenkinsProjectApplication.class);
+	//static Logger log=(Logger) LoggerFactory.getLogger(JenkinsProjectApplication.class);
 	
 	@Test
 	void contextLoads() {
